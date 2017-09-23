0dcbd146ed8e7e44ca5ef184706ed154414d2333
YellowBang368 <YellowBang@gmail.com>
2017-09-23 23:23:00 +0300

    Initial commit

---
+++ quiz.rb
@@ -0,0 +1,12 @@
+require "./methods.rb"
+
+puts "Пожалуйста отвечайте следующим образом:"
+puts "1.Да"
+puts "2.Нет"
+puts "3.Иногда"
+
+quiz = Quiz.new
+quiz.start
+printer = ResultPrinter.new
+printer.check_for(quiz.get_points)
+printer.show
